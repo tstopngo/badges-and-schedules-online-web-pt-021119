3896d96db9cdcecf52476ca8c6ba680375d490a6
Tracy Ngo <tngo1206@gmail.com>
2019-02-22 00:54:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+array_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#badge_maker(name)
+
+def batch_badge_creator(array)
+  array.map do |name|
+   badge_maker(name)
+  end
+end
+
+# batch_badge_creator(array)
+
+
+
+def assign_rooms(array)
+  array.map.each_with_index do |name,room|
+   "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+  end
+end 
+
+# assign_rooms(array)
+
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+    end
+   assign_rooms(array).each do |assign|
+    puts assign
+  end
+  end 
