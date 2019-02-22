a02a0d97fde3f01ef3cfe22a654c683ab0d7dabe
Tracy Ngo <tngo1206@gmail.com>
2019-02-22 01:23:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@
 array_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+   "Hello, my name is #{name}."
 end
 
 #badge_maker(name)
